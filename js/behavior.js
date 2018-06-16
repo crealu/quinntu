0fa224b786368be81dad3b965b2237c33050a423
Perios Dev <periosdev@Maccam.local>
2018-06-15 17:06:03 -0700

    Initial push
    Initial push

---
+++ js/behavior.js
@@ -0,0 +1,75 @@
+// quinn js
+
+function toggleNav() {
+  let navLinks = document.getElementById('mobile-nav-list');
+  if (!(navLinks.style.left === '0px')) {
+    navLinks.style.left = '0px';
+  } else {
+      navLinks.style.left = '150px';
+  }
+}
+
+function sayHi() {
+  let xinchao = document.getElementById('xinchao');
+  xinchao.style.opacity = 0;
+  var i = 1;
+  var id = setInterval(frame, 1000);
+  function frame() {
+    i--;
+    xinchao.style.opacity = i;
+    if (i == 0) {
+      if (xinchao.innerHTML == 'Xin chao') {
+        xinchao.innerHTML = 'Hello';
+      } else if (xinchao.innerHTML == 'Hello') {
+        xinchao.innerHTML = 'Xin chao';
+      }
+      clearInterval(id);
+      xinchao.style.opacity = 1;
+    }
+  }
+}
+
+function travel() {
+  let vietnam = document.getElementById('vietnam-map');
+  vietnam.style.opacity = 0;
+  var i = 1;
+  var id = setInterval(frame, 1000);
+  function frame() {
+    i--;
+    vietnam.style.opacity = i;
+    if (i == 0) {
+      if (vietnam.src === 'img/vietnam.jpg') {
+        vietnam.src = 'img/california.jpg';
+      } else if (vietnam.src === 'img/california.jpg') {
+        vietnam.src = 'img/vietnam.jpg';
+      }
+      clearInterval(id);
+      vietnam.style.opacity = 1;
+    }
+  }
+}
+
+function changeMap() {
+  let vietnam = document.getElementById('vietnam-map');
+  vietnam.style.opacity = 0;
+  vietnam.src = 'img/california.jpg';
+  vietnam.style.opacity = 1;
+}
+
+
+$(window).bind('scroll',function(e){
+  parallaxScroll();
+});
+
+function parallaxScroll(){
+  var scrolled = $(window).scrollTop();
+  $('#parallax-2').css('top',(0 - (scrolled * .25)) + ' px');
+  $('#parallax-3').css('top',(0 - (scrolled * .15)) + ' px');
+  $('#parallax-4').css('top',(0 - (scrolled * .15)) + ' px');
+  $('#parallax-5').css('top',(0 - (scrolled * .15)) + ' px');
+}
+
+$('#parallax-2').parallax({imageSrc: 'img/like1.JPG'});
+$('#parallax-3').parallax({imageSrc: 'img/like3.JPG'});
+$('#parallax-4').parallax({imageSrc: 'img/indo.JPG'});
+$('#parallax-5').parallax({imageSrc: 'img/gallax.JPG'});
